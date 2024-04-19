efcef67fa062ec8e56b9ada4630ddb7c20ebea20
Yoann <yoann.chamillard@gmail.com>
2024-04-19 16:25:01 +0200

    252.Modifiers

--- app/src/main/java/com/example/compose/MainActivity.kt
+++ app/src/main/java/com/example/compose/MainActivity.kt
@@ -4,10 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.requiredWidth
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -16,6 +25,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.compose.ui.theme.ComposeTheme
 
 class MainActivity : ComponentActivity() {
@@ -24,28 +34,26 @@ class MainActivity : ComponentActivity() {
         setContent {
             Column(
                 modifier = Modifier
-                    .fillMaxSize()
-                    .background((Color.Green)),
-                horizontalAlignment = Alignment.CenterHorizontally,
-                verticalArrangement = Arrangement.SpaceAround
+                    .background((Color.Green))
+                    .fillMaxHeight(0.5f)
+                    .fillMaxWidth(10f)
+                    .padding(top = 16.dp)
+                    .border(5.dp, Color.Blue)
+                    .padding(5.dp)
+                    .border(5.dp, Color.Cyan)
+                    .padding(5.dp)
+                    .border(10.dp, Color.Yellow)
+                    .padding(10.dp)
             ) {
-                Text(text = "Hello")
-                Text(text = "Hellooo")
-                Text(text = "Helloooooo")
+                Text(text = "Hello", modifier = Modifier
+                    .offset(x=0.dp, y=20.dp)
+                    .border(5.dp, Color.Red)
+                    .padding(10.dp)
+                )
+                Spacer(modifier = Modifier
+                    .height(50.dp))
+                Text(text = "Tom Jedusor")
             }
-            /*
-            Row(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background((Color.Green)),
-                verticalAlignment = Alignment.CenterVertically,
-                horizontalArrangement = Arrangement.SpaceAround
-            ) {
-                Text(text = "Hello")
-                Text(text = "Hellooo")
-                Text(text = "Helloooooo")
-            }
-             */
         }
     }
 }
