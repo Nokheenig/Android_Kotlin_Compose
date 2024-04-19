7556179b1de020324e463e241775bfa8c976f299
Yoann <yoann.chamillard@gmail.com>
2024-04-19 17:29:02 +0200

    253.Creating an Image Composable

--- app/src/main/java/com/example/compose/MainActivity.kt
+++ app/src/main/java/com/example/compose/MainActivity.kt
@@ -3,56 +3,86 @@ package com.example.compose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.requiredWidth
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
-import com.example.compose.ui.theme.ComposeTheme
+import androidx.compose.ui.unit.sp
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            Column(
-                modifier = Modifier
-                    .background((Color.Green))
-                    .fillMaxHeight(0.5f)
-                    .fillMaxWidth(10f)
-                    .padding(top = 16.dp)
-                    .border(5.dp, Color.Blue)
-                    .padding(5.dp)
-                    .border(5.dp, Color.Cyan)
-                    .padding(5.dp)
-                    .border(10.dp, Color.Yellow)
-                    .padding(10.dp)
-            ) {
-                Text(text = "Hello", modifier = Modifier
-                    .offset(x=0.dp, y=20.dp)
-                    .border(5.dp, Color.Red)
-                    .padding(10.dp)
+            val painter = painterResource(id = R.drawable.steamnocchio)
+            val contentDescription = "This is a picture with SteamNocchio"
+            val title = "This is SteamNocchio"
+            
+            ImageCard(painter = painter, contentDescription = contentDescription, title = title)
+        }
+    }
+}
+
+@Composable
+fun ImageCard(
+    painter: Painter,
+    contentDescription: String,
+    title: String,
+    modifier: Modifier = Modifier
+){
+    Card (
+        modifier = modifier
+            .fillMaxWidth(0.5f)
+            .padding(16.dp),
+        shape = RoundedCornerShape(16.dp),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 16.dp
+        )
+    ){
+        Box(modifier = Modifier.height(300.dp)){
+            Image(painter = painter,
+                contentDescription = contentDescription,
+                contentScale = ContentScale.Crop)
+
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .background(
+                    Brush.verticalGradient(
+                        colors = listOf(
+                            Color.Transparent,
+                            Color.Black
+                        ),
+                        startY = 300f
+                    )
                 )
-                Spacer(modifier = Modifier
-                    .height(50.dp))
-                Text(text = "Tom Jedusor")
+            )
+
+            Box(modifier = Modifier
+                .fillMaxSize()
+                .padding(12.dp),
+                contentAlignment = Alignment.BottomStart
+            ){
+                Text(
+                    text = title,
+                    style = TextStyle(color = Color.White, fontSize = 15.sp)
+                )
             }
         }
     }
