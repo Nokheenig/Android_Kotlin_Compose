8be50cc38b4509d3d0894b382d8f6468c5bcc97f
Yoann <yoann.chamillard@gmail.com>
2024-04-22 20:11:03 +0200

    257.TextFields, Buttons , Showing Snackbars

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,17 +30,17 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_19
+        targetCompatibility = JavaVersion.VERSION_19
     }
     kotlinOptions {
-        jvmTarget = "1.8"
+        jvmTarget = "19"
     }
     buildFeatures {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.5.1"
+        kotlinCompilerExtensionVersion = "1.5.2"
     }
     packaging {
         resources {
@@ -50,20 +50,50 @@ android {
 }
 
 dependencies {
-
+    //////////////////////////////////////////////////////
+    //Dependencies added at project creation:
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
-    implementation("androidx.activity:activity-compose:1.8.2")
-    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
     testImplementation("junit:junit:4.13.2")
+
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     androidTestImplementation(platform("androidx.compose:compose-bom:2023.08.00"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
+
+
+    implementation("androidx.activity:activity-compose:1.8.2")
+    implementation(platform("androidx.compose:compose-bom:2023.08.00"))
+    //implementation("androidx.compose.ui:ui")
+    implementation("androidx.compose.ui:ui-graphics")
+    implementation("androidx.compose.ui:ui-tooling-preview")
+    //implementation("androidx.compose.material3:material3")
+
+
+
+
+    
+    //////////////////////////////////////////////////////////////
+    //New and latest dependencies for compose :
+    implementation ("androidx.compose.animation:animation:1.6.6")
+    implementation ("androidx.compose.foundation:foundation:1.6.6")
+    implementation ("androidx.compose.material:material:1.6.6")
+    implementation ("androidx.compose.material3:material3:1.2.1")
+    implementation ("androidx.compose.runtime:runtime:1.6.6")
+    implementation ("androidx.compose.ui:ui:1.6.6")
+
+    // glance
+    // For AppWidgets support
+    //implementation ("androidx.glance:glance-appwidget:1.0.0")
+
+    // For Wear-Tiles support
+    //implementation ("androidx.glance:glance-wear-tiles:1.3.0")
+
+    // wear
+    //implementation ("androidx.wear.compose:compose-foundation:1.3.1")
+    //implementation ("androidx.wear.compose:compose-material:1.3.1")
+    //implementation ("androidx.wear.compose:compose-navigation:1.3.1")
+    // NOTE: DO NOT INCLUDE androidx.compose.material:material
 }
--- app/src/main/java/com/example/compose/MainActivity.kt
+++ app/src/main/java/com/example/compose/MainActivity.kt
@@ -1,64 +1,89 @@
 package com.example.compose
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.Button
+import androidx.compose.material.Scaffold
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.rememberScaffoldState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import kotlin.random.Random
+import androidx.compose.ui.unit.dp
+import kotlinx.coroutines.launch
 
 
 class MainActivity : ComponentActivity() {
+    @SuppressLint("UnusedMaterialScaffoldPaddingParameter")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         setContent {
-            Column (modifier = Modifier.fillMaxSize()){
-                val color = remember {
-                    mutableStateOf(Color.Blue)
+            val scaffoldState = rememberScaffoldState()
+            var textFieldState by remember {
+                mutableStateOf("")
+            }
+            val scope = rememberCoroutineScope()
+            Scaffold(
+                modifier = Modifier.fillMaxSize(),
+                scaffoldState = scaffoldState
+            ) {
+                Column(
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                    verticalArrangement = Arrangement.Center,
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(horizontal = 30.dp)
+                ) {
+                    TextField(
+                        value = textFieldState,
+                        label = {
+                            Text(text = "Enter your name")
+                        },
+                        onValueChange = {
+                            textFieldState = it
+                        },
+                        singleLine = true,
+                        modifier = Modifier.fillMaxWidth()
+                    )
+                    Spacer(modifier = Modifier.height(16.dp))
+                    Box(
+                        modifier = Modifier.fillMaxWidth(),
+                        contentAlignment = Alignment.CenterEnd
+                    ) {
+                        Button(
+                            onClick = {
+                                scope.launch {
+                                    scaffoldState.snackbarHostState.showSnackbar("Hello $textFieldState!")
+                                }
+                            }
+                        ) {
+                            Text(
+                                text = "Please greet me",
+                                color = Color.White
+                            )
+                        }
+                    }
                 }
-                ColorBox(Modifier
-                    .weight(1f)
-                    .fillMaxSize()){
-                    color.value = it
-                }
-
-                Box(modifier = Modifier
-                    .background(color.value) // <- the color that we get from our colorBox
-                    .weight(1f)
-                    .fillMaxSize()
-                )
             }
         }
     }
-}
-
-@Composable
-fun ColorBox(
-    modifier: Modifier = Modifier,
-    updateColor: (Color) -> Unit
-){
-    Box(modifier = modifier
-        .background(Color.Red)
-        .clickable {
-            updateColor(
-                Color(
-                    Random.nextFloat(),
-                    Random.nextFloat(),
-                    Random.nextFloat(),
-                    1f
-                )
-            )
-        }
-    )
 }
 
 
