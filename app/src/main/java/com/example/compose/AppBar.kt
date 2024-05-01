2755f2d8ef06ef672322411e9b8cd2ee5f26a8f2
Yoann <yoann.chamillard@gmail.com>
2024-05-01 23:28:41 +0200

    263.Navigation Drawer

---
+++ app/src/main/java/com/example/compose/AppBar.kt
@@ -0,0 +1,31 @@
+package com.example.compose
+
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.runtime.Composable
+
+@Composable
+fun AppBar(
+    onNavigationIconClick: () -> Unit
+) {
+    TopAppBar(
+        title = {
+            Text(text = "Navigation Compose App")
+        },
+        backgroundColor = MaterialTheme.colors.primary,
+        contentColor = MaterialTheme.colors.onPrimary,
+        navigationIcon = {
+            IconButton(onClick = { onNavigationIconClick() }) {
+                Icon(
+                    imageVector = Icons.Default.Menu,
+                    contentDescription = "Toggle Drawer"
+                )
+            }
+        }
+    )
+}
